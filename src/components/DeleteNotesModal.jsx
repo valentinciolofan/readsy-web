cad6e8ca60c3d8d06fb23a3cb6074e7e8c03448e
valentinciolofan <valentincatalin02@gmail.com>
2024-12-09 21:01:59 +0200

    Added functionality for delete mode buttons

--- src/components/DeleteNotesModal.jsx
+++ src/components/DeleteNotesModal.jsx
@@ -1,103 +1,157 @@
 import React, { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { deleteUserNotes } from '../firebase/firestore/notes/firestoreAsyncThunk';
 
 
-const DeleteNotesModal = () => {
-  const [animationClass, setAnimationClass] = useState("");
 
-  // Handle click outside the modal to trigger closing animation
-  const handleOutsideClick = (e) => {
-    const targetModal = e.target.closest("modal-overlay");
+const DeleteNotesModal = ({ deleteMode, deleteNotes, handleDeleteMode }) => {
+  const dispatch = useDispatch(state => state.notes.userNotes);
+  const [showModal, setShowModal] = useState(false);
 
-    // Set animation class based on screen size for closing animation
-    if (!targetModal) {
-      if (window.innerWidth <= 768) {
-        setAnimationClass("moveOutBottom");
-      } else {
-        setAnimationClass("fadeOut");
-      }
+  // const [animationClass, setAnimationClass] = useState("");
 
-      // After the animation, close the modal
-      setTimeout(() => {
-        closeModal(false);
-      }, 300);
+  // // Handle click outside the modal to trigger closing animation
+  // const handleOutsideClick = (e) => {
+  //   const targetModal = e.target.closest("modal-overlay");
+
+  //   // Set animation class based on screen size for closing animation
+  //   if (!targetModal) {
+  //     if (window.innerWidth <= 768) {
+  //       setAnimationClass("moveOutBottom");
+  //     } else {
+  //       setAnimationClass("fadeOut");
+  //     }
+
+  //     // After the animation, close the modal
+  //     setTimeout(() => {
+  //       closeModal(false);
+  //     }, 300);
+  //   }
+  // };
+
+  // // Handle 'Escape' key to close the modal
+  // const handleKey = (e) => {
+  //   if (e.key === "Escape") {
+  //     if (window.innerWidth <= 768) {
+  //       setAnimationClass("moveOutBottom");
+  //     } else {
+  //       setAnimationClass("fadeOut");
+  //     }
+
+  //     setTimeout(() => {
+  //       closeModal(false);
+  //     }, 300);
+  //   }
+  // };
+
+  // useEffect(() => {
+  //   // Attach keydown event when modal is open
+  //   // if (showModal) {
+  //   //   window.addEventListener("keydown", handleKey);
+  //   // }
+
+  //   // Cleanup the event listener and animation class on unmount or close
+  //   return () => {
+  //     window.removeEventListener("keydown", handleKey);
+  //     setAnimationClass("");
+  //   };
+  // }, []);
+
+
+  // const handleImportFiles = () => {
+  //   setFiles(true);
+  //   if (window.innerWidth <= 768) {
+  //     setAnimationClass("moveOutBottom");
+  //   } else {
+  //     setAnimationClass("fadeOut");
+  //   }
+  //   setTimeout(() => {
+  //     closeModal(false);
+  //   }, 300);
+
+  // }
+
+  // Delete notes
+  const handleDeleteNotes = () => {
+    const noteIds = deleteNotes;
+    try {
+      const result = dispatch(deleteUserNotes(noteIds)).unwrap();
+
+
+      // Turn off the delete mode
+      handleDeleteMode();
+
+      // Close the delete modal 
+      setShowModal(false);
+
+
+    } catch (error) {
+      console.error('Failed to delete notes:', error);
     }
   };
 
-  // Handle 'Escape' key to close the modal
-  const handleKey = (e) => {
-    if (e.key === "Escape") {
-      if (window.innerWidth <= 768) {
-        setAnimationClass("moveOutBottom");
-      } else {
-        setAnimationClass("fadeOut");
-      }
-
-      setTimeout(() => {
-        closeModal(false);
-      }, 300);
+  const handleDeleteBtn = () => {
+    if (!showModal) {
+      setShowModal(true);
     }
-  };
-
-  useEffect(() => {
-    // Attach keydown event when modal is open
-    // if (showModal) {
-    //   window.addEventListener("keydown", handleKey);
-    // }
-
-    // Cleanup the event listener and animation class on unmount or close
-    return () => {
-      window.removeEventListener("keydown", handleKey);
-      setAnimationClass("");
-    };
-  }, []);
-
-
-  const handleImportFiles = () => {
-    setFiles(true);
-    if (window.innerWidth <= 768) {
-      setAnimationClass("moveOutBottom");
-    } else {
-      setAnimationClass("fadeOut");
-    }
-    setTimeout(() => {
-      closeModal(false);
-    }, 300);
-
   }
+
   return (
-    // showModal && (
-    <div
-      className="modal-overlay"
-      onClick={handleOutsideClick}
-    // style={{ display: showModal ? "flex" : "none" }}
-    >
-      <div className={`confirm-delete-modal ${animationClass}`}>
-        <div className="modal-message">
-          <h5>Confirm delete</h5>
-          <p className="font-small">Are you sure you want to delete 34 notes?</p>
-        </div>
-
-        <div className="confirm-delete-actions">
+    <>
+      {deleteMode && (
+        <div className="delete-mode-modal">
+          {/* <DeleteNotesModal /> */}
           <button
             type="button"
-            onClick={() => console.log('check')}
-            id="deleteNotesBtn"
+            onClick={handleDeleteBtn}
+            className="delete-notes-btn btn-warning rounded"
           >
-            <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24"><path fill="none" stroke="currentColor" strokeDasharray={24} strokeDashoffset={24} strokeLinecap="round" strokeLinejoin="round" strokeWidth={4} d="M5 11l6 6l10 -10"><animate fill="freeze" attributeName="stroke-dashoffset" dur="0.4s" values="24;0"></animate></path></svg>
+            <svg xmlns="http://www.w3.org/2000/svg" width="1.5em" height="1.5em" viewBox="0 0 14 14"><path fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" d="M1 3.5h12m-10.5 0h9v9a1 1 0 0 1-1 1h-7a1 1 0 0 1-1-1zm2 0V3a2.5 2.5 0 1 1 5 0v.5m-4 3.001v4.002m3-4.002v4.002" /></svg>
           </button>
           <button
             type="button"
-            onClick={() => console.log('close')}
-            id="cancelDeleteNotesBtn"
+            onClick={handleDeleteMode}
+            className="closeNotesDeleteMode btn-primary rounded"
           >
-            <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24"><path fill="currentColor" d="M6.4 19L5 17.6l5.6-5.6L5 6.4L6.4 5l5.6 5.6L17.6 5L19 6.4L13.4 12l5.6 5.6l-1.4 1.4l-5.6-5.6z"></path></svg>
+            <svg xmlns="http://www.w3.org/2000/svg" width="1.5em" height="1.5em" viewBox="0 0 24 24"><path fill="currentColor" d="M19 6.41L17.59 5L12 10.59L6.41 5L5 6.41L10.59 12L5 17.59L6.41 19L12 13.41L17.59 19L19 17.59L13.41 12z"></path></svg>
           </button>
 
         </div>
-      </div>
-    </div>
+      )}
+      {showModal && (
+        <div
+          className="modal-overlay"
+        // onClick={handleOutsideClick}
+        // style={{ display: showModal ? "flex" : "none" }}
+        >
+          <div className={`confirm-delete-modal `}>
+            <div className="modal-message">
+              <h5>Confirm delete</h5>
+              <p className="font-small">Are you sure you want to delete {deleteNotes.length} notes?</p>
+            </div>
+
+            <div className="confirm-delete-actions">
+              <button
+                type="button"
+                onClick={handleDeleteNotes}
+                id="deleteNotesBtn"
+              >
+                <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24"><path fill="none" stroke="currentColor" strokeDasharray={24} strokeDashoffset={24} strokeLinecap="round" strokeLinejoin="round" strokeWidth={4} d="M5 11l6 6l10 -10"><animate fill="freeze" attributeName="stroke-dashoffset" dur="0.4s" values="24;0"></animate></path></svg>
+              </button>
+              <button
+                type="button"
+                onClick={() => { handleDeleteMode(); setShowModal(false); }}
+                id="cancelDeleteNotesBtn"
+              >
+                <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24"><path fill="currentColor" d="M6.4 19L5 17.6l5.6-5.6L5 6.4L6.4 5l5.6 5.6L17.6 5L19 6.4L13.4 12l5.6 5.6l-1.4 1.4l-5.6-5.6z"></path></svg>
+              </button>
+
+            </div>
+          </div>
+        </div>
+      )}
+    </>
   )
-  //   );
 };
 
 export default DeleteNotesModal;
--- src/components/Notes.jsx
+++ src/components/Notes.jsx
@@ -103,33 +103,27 @@ const Notes = () => {
         }
         setDeleteMode(!deleteMode);
     }
-    // Delete notes
-    const handleDeleteNotes = () => {
-        const noteIds = deleteNotes;
-        try {
-            const result = dispatch(deleteUserNotes(noteIds)).unwrap();
-            console.log('Notes deleted successfully:', result);
-        } catch (error) {
-            console.error('Failed to delete notes:', error);
-        }
-    };
+
     const handleSelectedNotes = (noteId) => {
         setDeleteNotes(prev =>
             prev.includes(noteId) ? prev.filter(id => id !== noteId) : [...prev, noteId]
         );
     };
 
-    // Select all notes when in delete mode
+    // Select all / deselect all notes when in delete mode
     const handleSelectAll = () => {
-        const allNotesId = userNotes.map(note => note.id);
-        setDeleteNotes(allNotesId);
+        console.log(deleteNotes.length)
+        if (deleteNotes.length === 0) {
+            const allNotesId = userNotes.map(note => note.id);
+            setDeleteNotes(allNotesId);
+        } else if (deleteNotes.length > 0) {
+            const allNotesId = userNotes.map(note => note.id);
+            setDeleteNotes([]);
+        }
+
     };
 
-    // Deselect all notes when in delete mode 
-    const handleDeselectAll = () => {
-        const allNotesId = userNotes.map(note => note.id);
-        setDeleteNotes([]);
-    };
+
 
     // useEffect(() => {
     //     window.addEventListener('click', (e) => {
@@ -144,27 +138,39 @@ const Notes = () => {
                 <div className="main-container">
                     <h3>My Notes</h3>
                     <div className="search-and-import">
-                        <div className="search-box-container">
-                            <svg className="search-box-icon" xmlns="http://www.w3.org/2000/svg" width="1.5em" height="1.5em" viewBox="0 0 24 24"><path fill="#909090" d="m19.6 21l-6.3-6.3q-.75.6-1.725.95T9.5 16q-2.725 0-4.612-1.888T3 9.5t1.888-4.612T9.5 3t4.613 1.888T16 9.5q0 1.1-.35 2.075T14.7 13.3l6.3 6.3zM9.5 14q1.875 0 3.188-1.312T14 9.5t-1.312-3.187T9.5 5T6.313 6.313T5 9.5t1.313 3.188T9.5 14"></path></svg>
-                            <input
-                                type="search"
-                                placeholder="Search by name of the document"
-                                className="search-box" />
-                        </div>
-                        <div>
+                        {deleteMode ? (
                             <button
                                 type="button"
-                                className="createNote btn-primary rounded"
-                                style={{ display: userNotes ? "block" : "none" }}
-                                onClick={handleAddNote}
+                                onClick={handleSelectAll}
                             >
-                                <svg xmlns="http://www.w3.org/2000/svg" width="1.5em" height="1.5em" viewBox="0 0 24 24"><path fill="currentColor" d="M11 13H5v-2h6V5h2v6h6v2h-6v6h-2z"></path></svg>
+                                Select All
                             </button>
-                            <button onClick={handleDeleteMode}>
-                                Delete
-                            </button>
-                        </div>
+                        ) : (
+                            <>
+                                <div className="search-box-container">
+                                    <svg className="search-box-icon" xmlns="http://www.w3.org/2000/svg" width="1.5em" height="1.5em" viewBox="0 0 24 24"><path fill="#909090" d="m19.6 21l-6.3-6.3q-.75.6-1.725.95T9.5 16q-2.725 0-4.612-1.888T3 9.5t1.888-4.612T9.5 3t4.613 1.888T16 9.5q0 1.1-.35 2.075T14.7 13.3l6.3 6.3zM9.5 14q1.875 0 3.188-1.312T14 9.5t-1.312-3.187T9.5 5T6.313 6.313T5 9.5t1.313 3.188T9.5 14"></path></svg>
+                                    <input
+                                        type="search"
+                                        placeholder="Search by name of the document"
+                                        className="search-box" />
+                                </div>
+                                <div>
+                                    <button
+                                        type="button"
+                                        className="createNote btn-primary rounded"
+                                        style={{ display: userNotes ? "block" : "none" }}
+                                        onClick={handleAddNote}
+                                    >
+                                        <svg xmlns="http://www.w3.org/2000/svg" width="1.5em" height="1.5em" viewBox="0 0 24 24"><path fill="currentColor" d="M11 13H5v-2h6V5h2v6h6v2h-6v6h-2z"></path></svg>
+                                    </button>
+
+                                </div>
+                            </>
+                        )}
                     </div>
+
+
+
                     {/* grid for notes */}
                     <div
                         ref={notesContainerRef}
@@ -206,40 +212,11 @@ const Notes = () => {
                             </label>
                         ))}
                     </div>
-                    {deleteMode ?
-                        <div>
-                            {/* <DeleteNotesModal /> */}
-                            <button
-                                type="button"
-                                // onClick={deleteSelectedNotes}
-                                className="delete-notes-btn"
-                            >
-                                <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24"><path fill="currentColor" d="M9 16.17L4.83 12l-1.42 1.41L9 19L21 7l-1.41-1.41z"></path></svg>
-                            </button>
-                            <button
-                                type="button"
-                                onClick={handleDeleteNotes}
-                                className="closeNotesDeleteMode"
-                            >
-                                <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24"><path fill="currentColor" d="M19 6.41L17.59 5L12 10.59L6.41 5L5 6.41L10.59 12L5 17.59L6.41 19L12 13.41L17.59 19L19 17.59L13.41 12z"></path></svg>
-                            </button>
-                            <button
-                                type="button"
-                                onClick={handleSelectAll}
-                            >
-                                Select All
-                            </button>
-                            <button
-                                type="button"
-                                onClick={handleDeselectAll}
-                            >
-                                Deselect All
-                            </button>
-
-                        </div>
-                        :
-                        ''
-                    }
+                    <DeleteNotesModal
+                        deleteMode={deleteMode}
+                        deleteNotes={deleteNotes}
+                        handleDeleteMode={handleDeleteMode}
+                    />
                 </div>
             ) : (
                 <div className="dashboard-zero-files">
